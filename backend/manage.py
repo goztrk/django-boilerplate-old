39362abebcac36d2e23ea467406e7cea2654f3c3
goztrk <me@gokhan.org.tr>
2023-04-13 10:47:49 +0300

    Add env.example

--- backend/manage.py
+++ backend/manage.py
@@ -1,14 +1,30 @@
 #!/usr/bin/env python
 """Django's command-line utility for administrative tasks."""
+# Python Standard Library
 import os
 import sys
+
+# Third Party (PyPI) Imports
+from decouple import config
+
+# isort: off
 
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', '{{ project_name }}.settings')
+    settings_module = config("DJANGO_SETTINGS_MODULE", default=None)
+
+    if sys.argv[1] == "test":
+        if settings_module:
+            print(
+                "Ignoring config('DJANGO_SETTINGS_MODULE') because it's test. "
+                "Using '{{project_name}}.settings.test'"
+            )
+        os.environ.setdefault("DJANGO_SETTINGS_MODULE", "{ project_name }}.settings.test")
+    else:
+        os.environ.setdefault("DJANGO_SETTINGS_MODULE", settings_module)
     try:
-        from django.core.management import execute_from_command_line
+        from django.core.management import execute_from_command_line  # pylint: disable=C0415
     except ImportError as exc:
         raise ImportError(
             "Couldn't import Django. Are you sure it's installed and "
@@ -18,5 +34,5 @@ def main():
     execute_from_command_line(sys.argv)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
