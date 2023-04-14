4cf492cb1e1f9f0c25f2622cc0a97e1b04f550c7
goztrk <me@gokhan.org.tr>
2023-04-14 23:18:43 +0300

    Fix vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,21 @@
 import { defineConfig } from 'vite';
+import path from 'path';
 import react from '@vitejs/plugin-react';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
 // https://vitejs.dev/config/
+/** @type {import('vite').UserConfig} */
 export default defineConfig({
   plugins: [react(), tsconfigPaths()],
+  root: path.resolve(__dirname, 'frontend'),
+  base: 'static/',
+  build: {
+    manifest: true,
+    outDir: path.resolve(__dirname, 'public/js'),
+    rollupOptions: {
+      input: {
+        app: 'apps/home/app.tsx'
+      },
+    },
+  },
 });
