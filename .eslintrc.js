388fa7b57028ddff30c0066a391141e1e1ceef78
goztrk <me@gokhan.org.tr>
2023-04-16 02:44:21 +0300

    Add import sort eslint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,17 +3,48 @@ module.exports = {
   extends: [
     'airbnb',
     'airbnb-typescript',
-    'prettier',
     'plugin:@typescript-eslint/recommended',
     'plugin:import/typescript',
+    'plugin:prettier/recommended',
   ],
-  plugins: [
-    '@typescript-eslint',
-    'import',
-    'prettier',
-  ],
+  plugins: ['@typescript-eslint', 'import', 'prettier', 'simple-import-sort'],
   parser: '@typescript-eslint/parser',
   parserOptions: {
     project: './tsconfig.eslint.json',
+  },
+  rules: {
+    'simple-import-sort/exports': 'error',
+    'simple-import-sort/imports': [
+      'error',
+      {
+        groups: [
+          // Node.js builtins
+          // eslint-disable-next-line global-require, @typescript-eslint/no-var-requires
+          [`^(${require('module').builtinModules.join('|')})(/|$)`],
+          // Packages. `react` related packages come first.
+          ['^react', '^@?\\w'],
+          // Side effect imports.
+          ['^\\u0000'],
+          // Internal Imports
+          [
+            // eslint-disable-next-line global-require, @typescript-eslint/no-var-requires
+            `^(${require('fs')
+              .readdirSync('frontend', { withFileTypes: true })
+              .filter((x) => x.isDirectory())
+              .map((x) => x.name)
+              .join('|')})(/.*|$)`,
+          ],
+          // Parent imports. Put `..` last.
+          ['^\\.\\.(?!/?$)', '^\\.\\./?$'],
+          // Other relative imports. Put same-folder imports and `.` last.
+          ['^\\./(?=.*/)(?!/?$)', '^\\.(?!/?$)', '^\\./?$'],
+          // Style imports.
+          ['^.+\\.s?css$'],
+        ],
+      },
+    ],
+    'import/first': 'error',
+    'import/newline-after-import': 'error',
+    'import/no-duplicates': 'error',
   },
 };
